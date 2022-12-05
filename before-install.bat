75c6ed0503af9d7c34effadb8673fc12e640af0f
DhirajGitHub0910 <116726913+DhirajGitHub0910@users.noreply.github.com>
2022-12-05 11:04:04 +0530

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,2 +1,2 @@
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command InstallWindowsFeature Web-Server
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
